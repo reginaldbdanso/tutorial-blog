ec0e15c735be45092dae0a5ecec1f7f073e44f67
reginaldbdanso <reginaldbdanso@gmail.com>
2024-01-05 16:23:24 +0000

    Firestore Ref
    Created a reference to firebase/firestore as const db
    and exported it.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
 createRoot(
   document.getElementById('root')).render(
