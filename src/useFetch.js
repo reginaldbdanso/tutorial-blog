9ac5116a49364ca94270c27cf3d9377315150ca2
reginaldbdanso <reginaldbdanso@gmail.com>
2024-01-05 16:24:12 +0000

    Firestore get method
    Replaced fetch API get method with firestore
    method. useFetch now able to get all blogs from firestore,
    or a particular blod when passed an id argument.

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,40 +1,72 @@
 import { useState, useEffect } from 'react';
+import { db } from './index.js';
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
 
-const useFetch = (url) => {
+const useFetch = (id=null) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const abortCont = new AbortController();
+    // const abortCont = new AbortController();
 
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-      .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-      })
-      .then(data => {
+    // setTimeout(() => {
+      if (id === null) {
+        // Give us all the data in the "blogs" collection
+      const querySnapshot = getDocs(collection(db, "blogs"));
+      querySnapshot.then((querySnapshot) => {
+        // console.log(querySnapshot.docs.forEach(doc => console.log(doc.data())))
+        const blogs = querySnapshot.docs.map(doc => {
+          return { ...doc.data(), id: doc.id }
+        });
+        // console.log(blogs);
+        setData(blogs);
         setIsPending(false);
-        setData(data);
         setError(null);
-      })
-      .catch(err => {
-        if (err.name === 'AbortError') {
-          console.log('fetch aborted')
-        } else {
-          // auto catches network / connection error
+      });
+    } else {
+      // Give us the data in the "blogs" collection with the id of "id"
+      const docRef = doc(db, "blogs", id);
+      const docSnap = getDoc(docRef);
+      docSnap.then((doc) => {
+        if (doc.exists()) {
+          console.log("Document data:", doc.data());
+          setData(doc.data());
           setIsPending(false);
-          setError(err.message);
+          setError(null);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
         }
       })
-    }, 1000);
+    }
+
+    //   fetch(url, { signal: abortCont.signal })
+    //   .then(res => {
+    //     if (!res.ok) { // error coming back from server
+    //       throw Error('could not fetch the data for that resource');
+    //     } 
+    //     return res.json();
+    //   })
+    //   .then(data => {
+    //     setIsPending(false);
+    //     setData(data);
+    //     setError(null);
+    //   })
+    //   .catch(err => {
+    //     if (err.name === 'AbortError') {
+    //       console.log('fetch aborted')
+    //     } else {
+    //       // auto catches network / connection error
+    //       setIsPending(false);
+    //       setError(err.message);
+    //     }
+    //   })
+    // }, 1000);
 
     // abort the fetch
-    return () => abortCont.abort();
-  }, [url])
+    // return () => abortCont.abort();
+  }, [id])
 
   return { data, isPending, error };
 }
