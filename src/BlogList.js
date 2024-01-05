88508ce37ab9c6cad6ed28c837ef1509806f6174
reginaldbdanso <reginaldbdanso@gmail.com>
2024-01-05 16:41:15 +0000

    Added a message if no blogs exist

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 const BlogList = ({ blogs, title }) => {
     // const blogs = props.blogs;
-    
+    const noBlogs = blogs.length === 0;
     return (
         <div className="blog-list">
             <h1>{title}</h1>
+            {noBlogs && <p>No Blogs Added. Create a ne blog</p>}
             {blogs.map((blog) => (
                     <div className="blog-preview" key={blog.id}>
                         <Link to={`/blogs/${blog.id}`}>
