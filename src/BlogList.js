2f3cd734ba178c32ff3339edb00d53a9fa74b2ba
reginaldbdanso <reginaldbdanso@gmail.com>
2024-01-08 11:26:18 +0000

    Fixed typo

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,7 +6,7 @@ const BlogList = ({ blogs, title }) => {
         <div className="blog-list">
             <h1>{title}</h1>
             {/* This is to display a message when there are no blogs in the database */}
-            {noBlogs && <p>No Blogs Added. Create a ne blog</p>}
+            {noBlogs && <p>No Blogs Added. Create a new blog</p>}
             {blogs.map((blog) => (
                     <div className="blog-preview" key={blog.id}>
                         <Link to={`/blogs/${blog.id}`}>
